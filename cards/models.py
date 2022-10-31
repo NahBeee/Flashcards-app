19b5d45ddb89315e556b3bb41d55693d13fb9d96
NahBeee <nahbee118@gmail.com>
2022-10-31 17:15:16 +1100

    finished

--- cards/models.py
+++ cards/models.py
@@ -6,15 +6,15 @@ from secrets import choice
 from unittest.util import _MAX_LENGTH
 from django.db import models
 
-num_boxes =5
-boxes = range(1,num_boxes +1)
+NUM_BOXES =5
+BOXES = range(1,NUM_BOXES +1)
 
 class Card(models.Model):
     question = models.CharField(max_length= 100) #the maximum length of the field.
     answer= models.CharField(max_length=100)
     box= models.IntegerField(
-        choices= zip(boxes, boxes),
-        default= boxes[0],
+        choices= zip(BOXES, BOXES),
+        default= BOXES[0],
     )
     date_created= models.DateTimeField(auto_now_add=True)
 
@@ -22,9 +22,9 @@ class Card(models.Model):
         return self.question
 
     def move(self,solved):
-        new_box= self.box + 1 if solved else boxes[0]
+        new_box= self.box + 1 if solved else BOXES[0]
 
-        if new_box in boxes:
+        if new_box in BOXES:
             self.box= new_box
             self.save()
 
